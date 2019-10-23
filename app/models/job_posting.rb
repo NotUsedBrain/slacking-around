a302dbf2f5a1e11de7cfbbec6a341f94f92f982e
NotUsedBrain <notusedbrain@hotmail.com>
2019-10-23 04:28:17 +0200

    added missing semicolons

--- app/models/job_posting.rb
+++ app/models/job_posting.rb
@@ -1,6 +1,6 @@
 class JobPosting < ApplicationRecord
     belongs_to :employer
     has_many :skills, through: :job_postings_skills
-    has_many :feedbacks, through :applications
+    has_many :feedbacks, through: :applications
     has_many :applications
 end
